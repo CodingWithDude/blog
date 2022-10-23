a94c8adb11ed8973e9fbec0c700f23c8fa156f25
CodingWithDude <codingwithdude@gmail.com>
2022-10-22 20:27:12 -0700

    Updated styling for header links

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,7 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { BsChatSquareDotsFill, BsTwitter } from 'react-icons/bs';
+import { MdOutlineWeb } from 'react-icons/md';
 
 const HeaderComponent = () => {
+  const styledLinkContainer: string =
+    'flex gap-4 text-lg group cursor-pointer text-gray-100 items-center';
+  const styledIcon: string =
+    'text-4xl bg-gradient-to-b from-cyan-500 group-hover:scale-105 to-green-500 ease-in duration-75 p-[7px] rounded-lg';
+
   return (
     <header className="flex flex-col mb-12 w-full max-w-[584px] ">
       <div className="flex gap-6">
@@ -18,20 +26,50 @@ const HeaderComponent = () => {
         </div>
         <div className="flex flex-col justify-center">
           <h1 className="text-gray-50 text-4xl">Ryan</h1>
-          <a
+          <Link
             href="https://codingwithdude.com"
-            className="cursor-pointer"
+            passHref
           >
-            <p className="text-gray-400 mt-1">Coding With Dude</p>
-          </a>
+            <p className="text-gray-400 mt-1 cursor-pointer">
+              Coding With Dude
+            </p>
+          </Link>
         </div>
       </div>
-
       <p className="mt-6 text-gray-100 ">
         Welcome to my digital playground where I share what I&apos;m learning
         about exciting new technologies and building a career as a self taught
         developer.
       </p>
+      <div className="flex gap-6 font-medium my-12">
+        <Link
+          href="https://codingwithdude.com"
+          passHref
+        >
+          <div className={styledLinkContainer}>
+            <BsChatSquareDotsFill className={styledIcon} />
+            <p>Posts</p>
+          </div>
+        </Link>
+        <Link
+          href="https://codingwithdude.com"
+          passHref
+        >
+          <div className={styledLinkContainer}>
+            <BsTwitter className={styledIcon} />
+            <p>Twitter</p>
+          </div>
+        </Link>
+        <Link
+          href="https://codingwithdude.com"
+          passHref
+        >
+          <div className={styledLinkContainer}>
+            <MdOutlineWeb className={`${styledIcon} p-[3px]`} />
+            <p>Portfolio</p>
+          </div>
+        </Link>
+      </div>
     </header>
   );
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import BackgroundFilter from './BackgroundFilter';
 import NavComponent from './NavComponent';
 import { useWindowScroll } from 'react-use';
 
 const Layout = ({ children }: { children: ReactElement }) => {
+  const [showNav, setShowNav] = useState(false);
+
   const GradientBackground = () => {
     const { y } = useWindowScroll();
 
@@ -12,7 +14,7 @@ const Layout = ({ children }: { children: ReactElement }) => {
       <>
         <div className="pointer-events-none absolute inset-0 overflow-hidden">
           <div
-            className="h-full bg-[url('https://res.cloudinary.com/djjxydn3p/image/upload/h_500/blog/bg_gradient.png')] bg-top bg-no-repeat opacity-[0.2] will-change-transform"
+            className="h-full transition-all ease-in bg-[url('https://res.cloudinary.com/djjxydn3p/image/upload/h_500/blog/bg_gradient.png')] bg-top bg-no-repeat opacity-[0.2] will-change-transform"
             style={{
               transform: `translateY(${Math.min(y / 3, 167)}px)`,
             }}
