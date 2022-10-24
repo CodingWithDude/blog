10e221d695be77e0cb91299122e46ee8cd832895
CodingWithDude <codingwithdude@gmail.com>
2022-10-23 20:12:16 -0700

    Added hover styling to NavLinks and profile pic

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -6,16 +6,18 @@ const HeaderComponent = () => {
   return (
     <header className="flex flex-col my-48 w-full max-w-[700px] ">
       <div className="flex gap-6">
-        <div className="items-center justify-center flex w-[90px] h-[90px] rounded-full bg-stone-600/30">
-          <div className="items-center cursor-pointer justify-center flex w-[80px] h-[80px] rounded-full bg-gradient-to-b from-cyan-500 to-green-500">
-            <Image
-              src="https://res.cloudinary.com/djjxydn3p/image/upload/v1666480691/profile-white-bg-500px_d9qlcq.png"
-              alt="profile picture"
-              className="rounded-full"
-              width={70}
-              height={70}
-            />
-          </div>
+        <div className="items-center group hover:scale-105 justify-center flex w-[90px] h-[90px] rounded-full bg-stone-600/30">
+          <Link href="/">
+            <div className="items-center cursor-pointer justify-center flex w-[80px] h-[80px] rounded-full bg-gradient-to-b from-cyan-500 to-green-500 group-hover:bg-gradient-to-b group-hover:from-purple-500 group-hover:to-cyan-500 group-hover:duration-1000">
+              <Image
+                src="https://res.cloudinary.com/djjxydn3p/image/upload/v1666480691/profile-white-bg-500px_d9qlcq.png"
+                alt="profile picture"
+                className="rounded-full"
+                width={70}
+                height={70}
+              />
+            </div>
+          </Link>
         </div>
         <div className="flex flex-col justify-center">
           <h1 className="text-gray-50 text-4xl">Ryan</h1>
--- src/components/NavComponent.tsx
+++ src/components/NavComponent.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { useWindowScroll } from 'react-use';
 import NavLinks from './NavLinks';
 
@@ -14,17 +15,19 @@ const NavComponent = ({ yScrollPosition, navShowYScrollPosition }: Props) => {
         yScrollPosition > navShowYScrollPosition ? '' : 'translate-y-[-110vh]'
       }`}
     >
-      <div className="items-center justify-center flex w-[54px] h-[54px] rounded-full bg-stone-600/30">
-        <div className="items-center cursor-pointer justify-center flex w-[47px] h-[47px] rounded-full bg-gradient-to-b from-cyan-500 to-green-500">
-          <Image
-            src="https://res.cloudinary.com/djjxydn3p/image/upload/v1666480691/profile-white-bg-500px_d9qlcq.png"
-            alt="profile picture"
-            className="rounded-full"
-            width={40}
-            height={40}
-          />
+      <Link href="/">
+        <div className="items-center justify-center flex w-[54px] h-[54px] rounded-full bg-stone-600/30">
+          <div className="items-center cursor-pointer justify-center flex w-[47px] h-[47px] rounded-full bg-gradient-to-b from-cyan-500 to-green-500">
+            <Image
+              src="https://res.cloudinary.com/djjxydn3p/image/upload/v1666480691/profile-white-bg-500px_d9qlcq.png"
+              alt="profile picture"
+              className="rounded-full"
+              width={40}
+              height={40}
+            />
+          </div>
         </div>
-      </div>
+      </Link>
       <NavLinks />
     </nav>
   );
